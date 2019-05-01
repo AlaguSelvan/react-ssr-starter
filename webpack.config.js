7f9860176acb79e05ed736505d6f3c5124047f1f
alaguselvangobumpr <alaguselvan@gobumpr.com>
2019-05-01 10:10:25 +0530

    hello world

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const ReactLoadablePlugin = require('react-loadable/webpack').ReactLoadablePlugi
 
 module.exports = {
   entry: {
-    client: './dist/client.js'
+    client: './lib/src/client.js'
   },
   output: {
     path: path.resolve(__dirname, 'public'),
