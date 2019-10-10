6a99ccbb3c51c37cb833e23b0bdd7633eefd4787
alaguselvangobumpr <alaguselvan@gobumpr.com>
2019-10-10 15:24:08 +0530

    express webpack dev server

--- src/app.js
+++ src/app.js
@@ -1,11 +1,18 @@
 import express from 'express'
 import Loadable from 'react-loadable'
+import helmet from 'helmet'
 import stateRoutes from './server/stateRoutes'
 import bodyParser from 'body-parser'
 import paths from '../config/paths'
+import routes from './client/Router/routes'
+import configureStore from './client/redux/configureStore'
+import MobileDetect from 'mobile-detect'
+import { matchPath } from 'react-router-dom'
+import ssr from './server/server'
 
 const app = express()
 const expressStaticGzip = require('express-static-gzip')
+app.use(helmet())
 
 require('dotenv').config()
 
@@ -23,4 +30,14 @@ Loadable.preloadAll().then(() => {
   })
 })
 
-stateRoutes(app)
+app.get('*', (req, res) => {
+  const store = configureStore()
+  const dataRequirementsDesktop = routes
+    .filter(route => matchPath(req.url, route))
+    .map(route => route.serverFetch ? route.serverFetch(store) : store)
+  Promise.all([dataRequirementsDesktop]).then(_ => {
+    const md = new MobileDetect(req.headers['user-agent'])
+    const response = ssr(req.url, store, md.mobile())
+    return res.send(response)
+  })
+})
